202f6ea2361a57b71f0cbcf069ca03310bdd2b4f
Frank Takang <frank.takang@levio.ca>
2024-09-23 21:25:08 -0400

    rds file update

--- rds.tf
+++ rds.tf
@@ -1,12 +1,12 @@
 # create database subnet group
 # terraform aws db subnet group
 resource "aws_db_subnet_group" "database_subnet_group" {
-  name         = "database subnet"
+  name         = "unique-database-subnet-group"
   subnet_ids   = [aws_subnet.private_data_subnet_az1.id, aws_subnet.private_data_subnet_az2.id ]
   description  = "subnets for database instance"
 
   tags   = {
-    Name = "database subnets"
+    Name = "unique-database-subnet-group"
   }
 }
 
@@ -14,15 +14,17 @@ resource "aws_db_subnet_group" "database_subnet_group" {
 # create the rds instance
 resource "aws_db_instance" "db_instance" {
 engine = "mysql"
-engine_version = "8.0.31"
+engine_version = "8.0.39"
 multi_az = false
 identifier = "dev-rds-instance"
 username = "ft11kn"
 password = "password123"
-instance_class = "db.t2.micro"
+instance_class = "db.t3.micro"
 allocated_storage = 200
 db_subnet_group_name = aws_db_subnet_group.database_subnet_group.name
 vpc_security_group_ids = [aws_security_group.database_security_group.id]
 availability_zone = "us-east-1b"
 db_name = "applicationdb"
+skip_final_snapshot = true
 }
+
